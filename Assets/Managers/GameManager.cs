1f194dfaa1ba2dec4d1bf5597424c9b109251677
MayankGameDev <35727794+mayanksoni1705@users.noreply.github.com>
2024-02-12 11:46:53 +0530

    Have to add player be able to send the data to server on button click

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -3,14 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ColyseusTicTacToe.ColyseusStates;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     private ColyseusRoom<MyRoomState> room;
+    private Dictionary<string, bool> players = new Dictionary<string, bool>();
+
+    [SerializeField] private List<Button> board = new List<Button>();
+    [SerializeField] private TextMeshProUGUI turnText;
+    [SerializeField] private GameObject waitingForPlayersText;
+
 
     async void Start()
     {
-        ColyseusClient client = new ColyseusClient("ws://192.168.0.183:2567");
+        ColyseusClient client = new ColyseusClient("ws://localhost:2567");
         room = await client.JoinOrCreate<MyRoomState>("my_room");
         Debug.Log("Connected to room: " + room.Id);
 
@@ -18,12 +27,75 @@ public class GameManager : MonoBehaviour
         {
             Debug.Log(message);
         });
-        room.State.OnTotalConnetedChange(OnTotalChange);    
-        room.Send("UpdateSession" , "myname = " + room.SessionId);
+        room.State.players.OnAdd((key, value) =>
+        {
+            if (!players.ContainsKey(key))
+            {
+                Debug.Log("Player joined: " + key);
+                players.Add(key, value);
+            }
+            else
+            {
+                Debug.Log("Player already exists: " + key);
+            }
+        });
+        room.State.players.OnRemove((key, value) =>
+        {
+            if (players.ContainsKey(key))
+            {
+                Debug.Log("Player left: " + key);
+                players.Remove(key);
+            }
+            else
+            {
+                Debug.Log("Player already removed" + key);
+            }
+        });
+        room.State.board.OnChange((key, value) =>
+        {
+            board[key].GetComponentInChildren<TextMeshProUGUI>().text = value.ToString();
+        });
+        room.State.OnCurrentTurnChange(OnCurrentTurnChange);
+        room.State.OnDrawChange(OnDrawChanged);
+        room.State.OnWinnerChange(OnWinnerChanged);
+
+        room.Send("UpdateSession", "myname = " + room.SessionId);
     }
 
-    private void OnTotalChange(float currentValue, float previousValue)
+    private void OnWinnerChanged(string currentValue, string previousValue)
     {
-        Debug.Log("Current Value: " + currentValue);
+        waitingForPlayersText.SetActive(true);
+        if (currentValue == room.SessionId)
+        {
+            waitingForPlayersText.GetComponent<TextMeshProUGUI>().text = "You won";
+        }
+        else
+        {
+            waitingForPlayersText.GetComponent<TextMeshProUGUI>().text = "You lost";   
+        }
+    }
+
+    private void OnDrawChanged(bool currentValue, bool previousValue)
+    {
+        if (currentValue == true)
+        {
+            waitingForPlayersText.SetActive(true);
+            waitingForPlayersText.GetComponent<TextMeshProUGUI>().text = "Draw";
+        }
+    }
+
+    private void OnCurrentTurnChange(string currentValue, string previousValue)
+    {
+        if (currentValue == room.SessionId)
+        {
+            waitingForPlayersText.SetActive(false);
+            Debug.Log("It's my turn");
+        }
+        else
+        {
+            waitingForPlayersText.SetActive(true);
+            waitingForPlayersText.GetComponent<TextMeshProUGUI>().text = "Waiting for opponent's turn";
+            Debug.Log("It's not my turn");
+        }
     }
 }
